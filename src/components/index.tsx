c5730ea7e30e9ff23fc0c27d24520d4870d406ed
cenxiaoxin <cenxiaoxin@forchange.tech>
2021-09-27 20:46:47 +0800

    feat: add immer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import produce from 'immer'
+import { useImmer } from 'use-immer'
 
 const Index = () => {
-  return <div>react comps</div>
+  const [obj, setObj] = useState<any>({
+    a: 1,
+    b: {
+      c: { d: 9 },
+    },
+  })
+  const [todo, setTodo] = useImmer({
+    a: 1,
+    b: {
+      c: { d: 9 },
+    },
+  })
+  const mutate = () => {
+    console.log('mutate')
+    setTodo((draft) => {
+      draft.b.c.d++
+    })
+    // setObj(
+    //   produce((draft: any) => {
+    //     console.log('draft', draft)
+    //     draft.a++
+    //   }),
+    // )
+  }
+  return <div onClick={mutate}>{JSON.stringify(todo)}</div>
 }
 
 export default Index
